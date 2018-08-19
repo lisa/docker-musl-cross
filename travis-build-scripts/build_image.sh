59dae7da86fec44b3f7946287bc382eede62c046
Lisa Seelye <lisa@users.noreply.github.com>
2018-08-19 17:08:33 -0400

    syntax errors, kill pid in err handler

--- travis-build-scripts/build_image.sh
+++ travis-build-scripts/build_image.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set +x
 set -e
@@ -7,7 +7,7 @@ set -e
 # Works around 4MB Travis log limit
 
 export PING_SLEEP=30s
-export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
+export WORKDIR="$( cd "$( dirname "$0" )" && pwd )"
 export BUILD_OUTPUT=$WORKDIR/build.out
 
 touch $BUILD_OUTPUT
@@ -17,17 +17,21 @@ dump_output() {
    tail -500 $BUILD_OUTPUT  
 }
 error_handler() {
+  local loop_pid=$1
   echo ERROR: An error was encountered with the build.
   dump_output
+  kill $loop_pid
   exit 1
 }
-# If an error occurs, run our error handler to output a tail of the build
-trap 'error_handler' EXIT
 
 # Set up a repeating loop to send some output to Travis.
 
 bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
+
+# If an error occurs, run our error handler to output a tail of the build
+trap "error_handler ${PING_LOOP_PID}" EXIT
+
 
 docker build -f Dockerfile -t thedoh/musl-cross:$TRAVIS_COMMIT . >> $BUILD_OUTPUT 2>&1
 
